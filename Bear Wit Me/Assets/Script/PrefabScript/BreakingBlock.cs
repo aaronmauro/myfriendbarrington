32045e7256ee6d4f320c0692837df0922f46a638
noahparfett <noahparfett@gmail.com>
2025-10-24 16:49:51 -0400

    Grey Box Level Design
    Concept scene for the grey box showcasing mechanics

--- Bear Wit Me/Assets/Script/PrefabScript/BreakingBlock.cs
+++ Bear Wit Me/Assets/Script/PrefabScript/BreakingBlock.cs
@@ -3,11 +3,13 @@ using UnityEngine;
 public class BreakingBlock:MonoBehaviour
 {
     public string playerTag = "Player";
+    [SerializeField] public float speed;
+
     private void OnCollisionEnter (Collision collision)
     {
         if (collision.gameObject.CompareTag(playerTag))
         {
-            Destroy(gameObject, 2f);
+            Destroy(gameObject, speed);
         }
     }
 }
