d724365dfa16c1dabfdc36142c9429d82058132d
Hiten <hp21zr@brocku.ca>
2025-10-24 16:32:38 -0400

    Hook Mechanic
    hook mechanic somewhat working with using the E button to activate hook in a certain angle

---
+++ Bear Wit Me/Assets/Script/Hook.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+
+public class Hook : MonoBehaviour
+{
+    [SerializeField] float hookForce = 25f;
+
+    Grapple grapple;
+    Rigidbody rigid;
+    LineRenderer lineRenderer;
+
+
+
+
+    public void Initialize(Grapple grapple, Transform shootTransform)
+    {
+        transform.forward = shootTransform.forward;
+        this.grapple = grapple;
+        rigid = GetComponent<Rigidbody>();
+        lineRenderer = GetComponent<LineRenderer>();
+        rigid.AddForce(transform.forward * hookForce, ForceMode.Impulse);
+    }
+
+
+
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3[] positions = new Vector3[]
+        {
+            transform.position,
+            grapple.transform.position
+        };
+
+        lineRenderer.SetPositions(positions);
+
+
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if((LayerMask.GetMask("Grapple") & 1 << other.gameObject.layer) > 0)
+        {
+            rigid.useGravity = false;
+            rigid.isKinematic = true;
+
+            grapple.StartPull();
+
+
+
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+}
