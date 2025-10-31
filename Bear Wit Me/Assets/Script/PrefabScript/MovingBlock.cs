c8a8a8c16478406bc1d5778011afc5e7f229db1a
noahparfett <noahparfett@gmail.com>
2025-10-31 12:34:16 -0400

    Moving Platform Fix
    Fixed moving platform to adjust player position

--- Bear Wit Me/Assets/Script/PrefabScript/MovingBlock.cs
+++ Bear Wit Me/Assets/Script/PrefabScript/MovingBlock.cs
@@ -4,13 +4,12 @@ public class NewMonoBehaviourScript : MonoBehaviour
 {
     public Transform targetA;
     public Transform targetB;
-    public Transform currentTarget;
+    private Transform currentTarget;
     public float speed = 0.5f;
     public float switchDistance = 0.05f;
 
     void Start()
     {
-      
         if (currentTarget == null)
         {
             currentTarget = targetA;
@@ -19,17 +18,34 @@ public class NewMonoBehaviourScript : MonoBehaviour
 
     void FixedUpdate()
     {
-        // Move towards the current target
+        
         transform.position = Vector3.MoveTowards(transform.position, currentTarget.position, speed * Time.deltaTime);
 
-        // Check distances
+       
         float distanceToCurrent = Vector3.Distance(transform.position, currentTarget.position);
 
-        // If close enough, switch to the other target
+       
         if (distanceToCurrent < switchDistance)
         {
             currentTarget = (currentTarget == targetA) ? targetB : targetA;
         }
     }
-}
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Make player follow the moving platform
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.transform.SetParent(transform);
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        // Detach player when they leave the platform
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.transform.SetParent(null);
+        }
+    }
+}
