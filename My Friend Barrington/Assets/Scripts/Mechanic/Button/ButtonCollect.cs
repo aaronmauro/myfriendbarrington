18ba917033270c6b227c942033a9f72872ecb163
noahparfett <noahparfett@gmail.com>
2025-11-14 15:17:45 -0500

    Added buttons and button counter UI
    Added buttons prefab for a breadcrumbing mechanic and button counter UI component that updates when buttons are collected

---
+++ My Friend Barrington/Assets/Scripts/Mechanic/Button/ButtonCollect.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using TMPro;
+
+public class ButtonCollect : MonoBehaviour
+{
+    // Shared across all instances
+    public static int buttonCount = 0;
+
+    [SerializeField]
+    public GameObject button;
+
+    public TMP_Text messageText;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player") && button != null)
+        {
+            button.SetActive(false);
+
+            buttonCount++;        // increases the global count
+            UpdateScore();
+        }
+    }
+
+    public void UpdateScore()
+    {
+        messageText.text = "Buttons: " + buttonCount;
+    }
+}
