3dc6fdc30b6609d6306ac707c4850d2b362fc55d
Epric096 <eric050225@gmail.com>
2026-01-23 21:45:25 -0500

    testing

--- My Friend Barrington/Assets/Scripts/Managers/SceneScript/MenuChangeScene.cs
+++ My Friend Barrington/Assets/Scripts/Managers/SceneScript/MenuChangeScene.cs
@@ -7,10 +7,28 @@ public class MenuChangeScene : MonoBehaviour
     private Button startButton;
     [SerializeField] 
     private Button optionButton;
+    [SerializeField]
+    private Button mainButton;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        //startButton.
+        // Finding Button
+        startButton = GameObject.Find("StartButton").GetComponent<Button>();
+        optionButton = GameObject.Find("OptionsButton").GetComponent<Button>();
+        mainButton = GameObject.Find("MainMenuButton").GetComponent<Button>();
+        if (startButton != null && optionButton != null)
+        {
+            startButton.onClick.AddListener(startGame);
+            optionButton.onClick.AddListener(optionMenu);
+        }
+        else if (mainButton != null)
+        {
+            optionButton.onClick.AddListener(optionMenu);
+        }
+        else
+        {
+            return;
+        }
     }
 
     // Update is called once per frame
@@ -18,14 +36,21 @@ public class MenuChangeScene : MonoBehaviour
     {
         
     }
-
+    // start game
     private void startGame()
     {
-
+        // going to level 1
+        SceneManagerScript.instance.nextScene("whiteboxLevel1");
     }
-
+    // option menu
     private void optionMenu()
     {
-
+        // going to option menu
+        SceneManagerScript.instance.nextScene("OptionsMenu");
+    }
+    // main menu
+    private void mainMenu()
+    {
+        SceneManagerScript.instance.nextScene("MainMenu(StartScreen)");
     }
 }
