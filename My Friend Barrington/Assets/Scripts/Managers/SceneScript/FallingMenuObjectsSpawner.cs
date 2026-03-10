b665dec2bb886dc6c2703ef8317d3a08c3046bf8
cyanLaStride <daniel@vidberg.com>
2026-03-10 07:39:26 -0400

    fun menu things
    back to work

--- My Friend Barrington/Assets/Scripts/Managers/SceneScript/FallingMenuObjectsSpawner.cs
+++ My Friend Barrington/Assets/Scripts/Managers/SceneScript/FallingMenuObjectsSpawner.cs
@@ -6,10 +6,13 @@ public class FallingMenuObjectsSpawner : MonoBehaviour
 
     [SerializeField] float spawnTime;
     [SerializeField] GameObject[] menuThings;
+    int item;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        item = Random.Range(0, menuThings.Length - 1);
+        SpawnItem();
         StartCoroutine(SpawnTimer());
     }
 
@@ -22,15 +25,14 @@ public class FallingMenuObjectsSpawner : MonoBehaviour
     private IEnumerator SpawnTimer()
     {
         yield return new WaitForSeconds(spawnTime);
-        Debug.Log("why");
         SpawnItem();
         StartCoroutine(SpawnTimer());
     }
 
     void SpawnItem()
     {
-        Debug.Log("plz");
-        Instantiate(menuThings[0], this.transform);
-        Debug.Log("you did it");
+        item += Random.Range(1, menuThings.Length - 1);
+        if (item >= 4) item -= 4;
+        Instantiate(menuThings[item], new Vector2(Random.Range(0, 1800), 1124), Quaternion.Euler(0, 0, Random.Range(0, 359)), this.transform);
     }
 }
