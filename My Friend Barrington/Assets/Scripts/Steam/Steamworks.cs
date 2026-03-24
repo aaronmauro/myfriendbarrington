9179f5b0f3b111a249acc39dc4afc1d94f261e51
Hiten Patel <hp21zr@brocku.ca>
2026-03-24 15:15:46 -0400

    Steam Manager and Steam Script
    Steam manger and steam users will pop up when game boots up in main menu

--- My Friend Barrington/Assets/Scripts/Steam/Steamworks.cs
+++ My Friend Barrington/Assets/Scripts/Steam/Steamworks.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
-
+using System.Collections;
+using Steamworks;
 public class Steamworks : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
