350949d971ec5f2d20b87ef34a0f8504f6e6fb3c
Hiten <hp21zr@brocku.ca>
2026-03-24 21:48:12 -0400

    Testing Achievements

---
+++ My Friend Barrington/Assets/Scripts/PlayerScript/SteamAchieve.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using Steamworks;
+
+public class SteamAchieve : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!SteamManager.Initialized) { return; }
+
+        if (!Input.GetKeyDown(KeyCode.Space)) { return; }
+
+        //SteamUserStats.ResetAllStats(true);   // use to reset stats when testing
+
+        SteamUserStats.SetAchievement("ACH_WIN_ONE_GAME");
+
+        SteamUserStats.StoreStats();
+
+    }
+}
--- My Friend Barrington/Assets/Scripts/Steam/SteamScript.cs
+++ My Friend Barrington/Assets/Scripts/Steam/SteamScript.cs
@@ -1,37 +1,15 @@
 using UnityEngine;
 using System.Collections;
 using Steamworks;
+
 public class SteamScript : MonoBehaviour
 {
-    protected Callback<GameOverlayActivated_t> m_GameOverlayActivated;
-
-
-    void Start()
+    private void Start()
     {
-        if (SteamManager.Initialized)
-        {
-            string name = SteamFriends.GetPersonaName();
-            Debug.Log(name);
-        }
-    }
+        if (!SteamManager.Initialized) { return; }
 
-    private void OnEnable()
-    {
-        if (SteamManager.Initialized)
-        {
-            m_GameOverlayActivated = Callback<GameOverlayActivated_t>.Create(OnGameOverlayActivated);
-        }
-    }
-
-    private void OnGameOverlayActivated(GameOverlayActivated_t pCallback)
-    {
-        if (pCallback.m_bActive != 0)
-        {
-            Debug.Log("Steam Overlay has been activated");
-        }
-        else
-        {
-            Debug.Log("Steam Overlay has been closed");
-        }
+        string name = SteamFriends.GetPersonaName();
+        
+        Debug.Log(name);
     }
 }
